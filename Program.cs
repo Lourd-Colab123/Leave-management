cbd085347a643e4f06c60b52974b2428dd8a9e60
Reguyal-DCET <laderashanah@gmail.com>
2026-02-21 18:37:24 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Leave_management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> fileleave = new List<string>();
+            string leavereason;
+
+      
+                Console.WriteLine("--------Leave Management system-------");
+                Console.Write("Enter A reason for leave: ");
+                leavereason = Console.ReadLine();
+
+                fileleave.Add(leavereason);
+
+                
+
+
+
+        }
+    }
+}
