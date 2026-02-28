6c63733ecaf881af34194036dce89ab876491c86
anon <lourdlealcruz@gmail.com>
2026-02-28 16:16:12 +0800

    Leave_Management2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,28 @@
 
       
                 Console.WriteLine("--------Leave Management system-------");
-                Console.Write("Enter A reason for leave: ");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Please Choose A Request Leave ");
+
+                Console.WriteLine();
+
+            Console.WriteLine("(Half Day Leave)");
+
+                Console.WriteLine("(1 Day Leave)");
+
+                Console.WriteLine("(3 Days Leave)");
+
+                Console.WriteLine("(5 Days Leave)");
+
+                Console.WriteLine("(Custom Leave)");
+
+                Console.WriteLine();
+
+            Console.Write("Enter Date:");
+
+
                 leavereason = Console.ReadLine();
 
                 fileleave.Add(leavereason);
