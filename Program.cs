3d4e1ce5adc8df631aa72fdf6b410809a08df406
Lourd-Colab123 <lourdlealcruz@gmail.com>
2026-03-26 20:55:28 +0800

    Leave_Management3

--- Program.cs
+++ Program.cs
@@ -4,41 +4,51 @@
     {
         static void Main(string[] args)
         {
-           List<string> fileleave = new List<string>();
+            List<string> fileleave = new List<string>();
             string leavereason;
 
-      
-                Console.WriteLine("--------Leave Management system-------");
+            Console.WriteLine("--------Leave Management system-------");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
 
-                Console.WriteLine();
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
 
-                Console.WriteLine("Please Choose A Request Leave ");
+            string correctPassword = "5 Dollars and 3 Cents";
 
-                Console.WriteLine();
+            if (password == correctPassword)
+            {
+                Console.WriteLine("Access Granted!");
+                Console.WriteLine("Welcome " + name);
 
-            Console.WriteLine("(Half Day Leave)");
+                Console.Write("Do you want to apply for leave? (y/n): ");
+                string applyLeave = Console.ReadLine().ToLower();
 
-                Console.WriteLine("(1 Day Leave)");
+                if (applyLeave == "y")
+                {
+                    Console.Write("Enter your leave reason: ");
+                    leavereason = Console.ReadLine();
+                    fileleave.Add(leavereason);
+                    Console.WriteLine("Your leave has been recorded: " + leavereason);
+                }
+                else
+                {
+                    Console.WriteLine("No leave was applied.");
+                }
 
-                Console.WriteLine("(3 Days Leave)");
+                Console.WriteLine("All leave requests:");
+                foreach (var leave in fileleave)
+                {
+                    Console.WriteLine("- " + leave);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Try Again Bro...");
+            }
 
-                Console.WriteLine("(5 Days Leave)");
-
-                Console.WriteLine("(Custom Leave)");
-
-                Console.WriteLine();
-
-            Console.Write("Enter Date:");
-
-
-                leavereason = Console.ReadLine();
-
-                fileleave.Add(leavereason);
-
-                
-
-
-
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
         }
     }
 }
